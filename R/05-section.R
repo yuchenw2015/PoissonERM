cc3788803964b488f44e5457d8ec9309d9d4e454
yuchenw2015 <37629284+yuchenw2015@users.noreply.github.com>
2023-11-10 17:04:42 -0800

    Update 05-section.R

--- R/05-section.R
+++ R/05-section.R
@@ -7,7 +7,7 @@ Conclusion <- function(title = NULL){
   write(conclusion_text,file=title,append=TRUE)
 
   for(jj in report.endpoints){
-    endpoint_conclusions_formatted <- paste(c("- ","A poisson regression model was developed for ",EndpointNameLatex(jj),"."),collapse="")
+    endpoint_conclusions_formatted <- paste(c("- ","A Poisson regression model was developed for ",EndpointNameLatex(jj),"."),collapse="")
     endpoint_conclusions_formatted <- c(endpoint_conclusions_formatted, Conclusions_endpoint(jj) )
     write(endpoint_conclusions_formatted,paste(glob.wd, "ReportComponents","Conclusions.txt", sep = "/"),append=T)
     if(!is.null(file)) write(endpoint_conclusions_formatted,file=title,append=TRUE)
